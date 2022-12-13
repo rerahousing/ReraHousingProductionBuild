1c34e3f3c221ad462b2f5f80b917b2057c01e880
Umang <umang965454@gmail.com>
2022-12-14 01:45:48 +0530

    commit 46

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ if (NODE_ENV == "production") {
   const path = require("path");
 
   app.use(express.static(path.join(__dirname, "../my-app", "build")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../my-app", "build", "index.html")),
       function (err) {
         if (err) {
